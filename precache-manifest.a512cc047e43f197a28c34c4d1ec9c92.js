d89a359593b80c2f87f4e9dff226bb756ef1aa8b
Deepak <DPK101@users.noreply.github.com>
2019-11-17 20:24:22 +0530

    Updates

---
+++ precache-manifest.a512cc047e43f197a28c34c4d1ec9c92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9021689ed65bd7dcbfaaf88293cc960",
+    "url": "/aboutme/index.html"
+  },
+  {
+    "revision": "2b1bac0bbf0c42a983a3",
+    "url": "/aboutme/static/css/main.2b3c9dfb.chunk.css"
+  },
+  {
+    "revision": "5475d916f147aeee2033",
+    "url": "/aboutme/static/js/2.7245fc01.chunk.js"
+  },
+  {
+    "revision": "2b1bac0bbf0c42a983a3",
+    "url": "/aboutme/static/js/main.79c49782.chunk.js"
+  },
+  {
+    "revision": "60b21b382a506fea5b68",
+    "url": "/aboutme/static/js/runtime-main.259db1e2.js"
+  }
+]);
